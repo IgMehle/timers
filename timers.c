56817f0455204906b828647e492f4b188458c295
IgMehle <igmehlegtr@gmail.com>
2026-04-25 17:08:45 -0300

    correcciones en manejo de memoria

--- timers.c
+++ timers.c
@@ -11,99 +11,102 @@
 /* -----------------------------------------------------
  * GLOBAL VARIABLES
  * -----------------------------------------------------*/
-volatile timer_t timers[N_TIMERS];
-volatile uint8_t timers_count = 0;
+static timer_t *timers = NULL;
+static uint8_t n_timers = 0;
+static uint8_t timers_count = 0;
 
 /* -----------------------------------------------------
  * FUNCTIONS
  * -----------------------------------------------------*/
-void init_timers(void)
+void timers_init(timer_t *my_timers, uint8_t n)
 {
-	timers_count = 0;
+    timers = my_timers;
+    if (n < MAX_N_TIMERS) n_timers = n;
+	else n_timers = MAX_N_TIMERS;
 }
 
 uint8_t give_timer(uint32_t time, uint8_t (*callback)(), uint8_t priority)
 {
 	uint8_t n_timer = 0xFF;
 	// Si no llegue al maximo de timers, agrego
-	if (timers_count < N_TIMERS) {
+	if (timers_count < n_timers) {
 		// Lleno los campos del nuevo timer
 		// Inicia apagado
-		timers[timers_count].name =     timers_count;
-		timers[timers_count].enabled =  0;
+		timers[timers_count].id =       timers_count;
         timers[timers_count].priority = priority;
+		timers[timers_count].enabled =  0;
+        timers[timers_count].pending =  0;
 		timers[timers_count].rep =      TIMER_PERIODIC;
-        timers[timers_count].event =    0;
+        timers[timers_count].callback = callback;
 		timers[timers_count].time =     time;
 		timers[timers_count].counter =  time;
-		timers[timers_count].callback = callback;
 		// Devuelvo el numero de timer
-		n_timer = timers[timers_count].name;
+		n_timer = timers[timers_count].id;
 		// Incremento la cantidad de timers creados
 		timers_count++;
 	}
 	return n_timer;
 }
 
-void on_timer(uint8_t name, uint8_t rep)
+void on_timer(uint8_t id, uint8_t rep)
 {
-	timers[name].counter = timers[name].time;
-	if (rep != 0) timers[name].rep = rep;
-	timers[name].enabled = 1;
+	timers[id].counter = timers[id].time;
+	if (rep != 0) timers[id].rep = rep;
+	timers[id].enabled = 1;
 }
 
-void pause_timer(uint8_t name)
+void pause_timer(uint8_t id)
 {
-	timers[name].enabled = 0;
+	timers[id].enabled = 0;
 }
 
-void continue_timer(uint8_t name)
+void continue_timer(uint8_t id)
 {
-	timers[name].enabled = 1;
+	timers[id].enabled = 1;
 }
 
-void reset_timer(uint8_t name)
+void reset_timer(uint8_t id)
 {
-	timers[name].counter = timers[name].time;
+	timers[id].counter = timers[id].time;
 }
 
-void set_timer_repeats(uint8_t name, uint8_t rep)
+void set_timer_repeats(uint8_t id, uint8_t rep)
 {
-    if (rep != 0) timers[name].rep = rep;
+    if (rep != 0) timers[id].rep = rep;
 }
 
-void add_timer_repeats(uint8_t name, uint8_t rep)
+void add_timer_repeats(uint8_t id, uint8_t rep)
 {
-    uint16_t sum_rep = rep + timers[name].rep;
+    uint16_t sum_rep = rep + timers[id].rep;
     if (rep != 0 && sum_rep != 0) {
-        if (sum_rep > TIMER_MAX_REP) timers[name].rep = TIMER_MAX_REP;
-        else timers[name].rep = (uint8_t) sum_rep;
+        if (sum_rep > TIMER_MAX_REP) timers[id].rep = TIMER_MAX_REP;
+        else timers[id].rep = (uint8_t) sum_rep;
     }
 }
 
-void resize_timer(uint8_t name, uint32_t time)
+void resize_timer(uint8_t id, uint32_t time)
 {
-	timers[name].time = time;
-	timers[name].counter = time;
+	timers[id].time = time;
+	timers[id].counter = time;
 }
 
-void off_timer(uint8_t name)
+void off_timer(uint8_t id)
 {
-	timers[name].enabled = 0;
-	timers[name].counter = timers[name].time;
+	timers[id].enabled = 0;
+	timers[id].counter = timers[id].time;
 }
 
-timer_t get_timer_status(uint8_t name)
+timer_t get_timer_status(uint8_t id)
 {
     timer_t tmr;
-    tmr.name =      timers[name].name;
-    tmr.enabled =   timers[name].enabled;
-    tmr.priority =  timers[name].priority;
-    tmr.rep =       timers[name].rep;
-    tmr.event =     timers[name].event;
-    tmr.time =      timers[name].time;
-    tmr.counter =   timers[name].counter;
-    tmr.callback =  timers[name].callback;
+    tmr.id =        timers[id].id;
+    tmr.priority =  timers[id].priority;
+    tmr.enabled =   timers[id].enabled;
+    tmr.rep =       timers[id].rep;
+    tmr.pending =   timers[id].pending;
+    tmr.time =      timers[id].time;
+    tmr.counter =   timers[id].counter;
+    tmr.callback =  timers[id].callback;
     return tmr;
 }
 
@@ -115,8 +118,8 @@ void timers_tick(void)
 		if (timers[i].enabled) {
 			// Si vencio el contador
 			if (timers[i].counter == 0) {
-                // levanto el flag de evento
-				timers[i].event = 1;
+                // levanto el flag de pending
+				timers[i].pending = 1;
 				// Si el timer es periodico, solo recargo el contador
 				if (timers[i].rep == TIMER_PERIODIC) {
                     timers[i].counter = timers[i].time;
@@ -136,19 +139,28 @@ void timers_tick(void)
 	}
 }
 
+void push_timers_queue(uint8_t id)
+{
+
+}
+
+void pop_timers_queue(uint8_t id)
+{
+
+}
+
 void timers_process(uint8_t priority)
 {
     uint8_t callback_status = TIMER_CALLBACK_OK;
     // Itero por todos los timers creados
 	for (uint8_t i = 0; i < timers_count; i++) {
-        // Si el flag de evento esta habilitado
+        // Si el flag de pending esta habilitado
         // y el timer tiene la prioridad especificada
-        if (timers[i].event && timers[i].priority == priority) {
+        if (timers[i].pending && timers[i].priority == priority) {
             // Llamo al callback
             callback_status = timers[i].callback();
-            // limpio flag de evento
-            timers[i].event = 0;
+            // limpio flag de pending
+            timers[i].pending = 0;
         }
     }
 }
-
--- timers.h
+++ timers.h
@@ -12,40 +12,41 @@
 
 // TIMER_T
 typedef struct timer {
-	uint8_t name;
-	uint8_t enabled;
-	uint8_t priority;
-	uint8_t event;
-    uint8_t rep;
+	uint8_t id;
+    uint8_t priority;
+	volatile uint8_t enabled;
+	volatile uint8_t pending;
+    volatile uint8_t rep;
 	uint8_t (*callback)(void);
     uint32_t time;
-	uint32_t count;
+	volatile uint32_t count;
 } timer_t;
 
-#define N_TIMERS		    16U
 #define MAX_N_TIMERS        64U
-#define TIMER_EVENT_ON      1U
-#define TIMER_EVENT_OFF     0U
+#define TIMER_PENDING_ON    1U
+#define TIMER_PENDING_OFF   0U
 #define TIMER_PERIODIC	    0xFF
 #define TIMER_MAX_REP       0xFE
 #define TIMER_CALLBACK_OK   0U
 
 // INIT - CONFIG
-void init_timers(void);
-uint8_t give_timer(uint32_t time, uint8_t (*callback)(), uint8_t priority)
+void timers_init(timer_t *my_timers, uint8_t n);
+uint8_t give_timer(uint32_t time, uint8_t (*callback)(), uint8_t priority);
 // TIMER MANIPULATION
-void on_timer(uint8_t name, uint8_t rep);
-void pause_timer(uint8_t name);
-void continue_timer(uint8_t name);
-void reset_timer(uint8_t name);
-void set_timer_priority(uint8_t name, uint8_t priority);
-void set_timer_repeats(uint8_t name, uint8_t rep);
-void add_timer_repeats(uint8_t name, uint8_t rep);
-void resize_timer(uint8_t name, uint32_t time);
-void off_timer(uint8_t name);
-timer_t get_timer_status(uint8_t name);
+void on_timer(uint8_t id, uint8_t rep);
+void pause_timer(uint8_t id);
+void continue_timer(uint8_t id);
+void reset_timer(uint8_t id);
+void set_timer_priority(uint8_t id, uint8_t priority);
+void set_timer_repeats(uint8_t id, uint8_t rep);
+void add_timer_repeats(uint8_t id, uint8_t rep);
+void resize_timer(uint8_t id, uint32_t time);
+void off_timer(uint8_t id);
+timer_t get_timer_status(uint8_t id);
 // TIMERS CORE
 void timers_tick(void);
+void push_timers_queue(uint8_t id);
+void pop_timers_queue(uint8_t id);
 void timers_process(uint8_t priority);
 
 #endif /* TIMERS_H_ */
